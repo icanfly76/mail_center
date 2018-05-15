ef9f81c64d134e890ef0538ea12f8fbce5090ba1
root <362854025@qq.com>
2018-05-15 18:07:03 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+$conf = array(
+    'redis' => array(
+        'host' => 'redis-ip',
+        'port' =>  '6379'
+    ),
+
+    'access_key' => array(
+        ''
+    ),
+    'allow_access_ip' => array(
+        '127.0.0.1',
+	'机器IP',
+
+    ),
+    'mail' => array(
+        'host' => 'smtp.qq.com',
+        'port' => 25,
+        'username' => '***',
+        'password' => '***',
+    ),
+);
